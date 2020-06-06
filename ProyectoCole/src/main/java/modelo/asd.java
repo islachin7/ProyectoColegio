c63cec3a71ddce8045d207830176de0948407e93
Islachin <islachinvictor7@gmail.com>
2020-06-06 17:58:40 -0500

    nuevo Cosa

---
+++ ProyectoCole/src/main/java/modelo/asd.java
@@ -0,0 +1,28 @@
+
+package modelo;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+public class asd {
+    public static void main(String[] args) {
+     reniec re = new reniec();
+        System.out.println(re.dni("76550656"));
+    }
+    public static void obtieneJson()
+{
+    
+    
+}
+   
+}
